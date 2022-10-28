9bdeb0075f7dfa24b9fc4aa6435990f2c73ed702
David Zhang <davidzhang2023@u.northwestern.edu>
2022-10-28 16:30:43 -0500

    connect to firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Machine from './components/Machine';
+import { useDbData } from './utilities/firebase';
+
 function App() {
+  const [machines, error] = useDbData('/');
+
+  if (machines == undefined) return <h1>Loading...</h1>
+  if (error) return <h1>Error loading data</h1>
+
   return (
     <div className="App">
+      <div>{machines["washer1"].startTime}</div>
       <Machine/>
     </div>
   );
