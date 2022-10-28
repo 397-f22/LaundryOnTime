e11500e12bb8a70a1e0b124d9f802289a0333e18
MCDK-Tech <davidkim2023@u.northwestern.edu>
2022-10-28 16:50:00 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Machine from './components/Machine';
 import { useDbData } from './utilities/firebase';
 
+
 function App() {
   const [machines, error] = useDbData('/');
 
   if (machines == undefined) return <h1>Loading...</h1>
   if (error) return <h1>Error loading data</h1>
+  
 
   return (
     <div className="App">
-      <div>{machines["washer1"].startTime}</div>
-      <Machine/>
+      <Machine machines = {machines}/>
     </div>
   );
 }
--- src/components/Machine.js
+++ src/components/Machine.js
@@ -9,7 +9,7 @@ const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
 const NOW_IN_MS = new Date().getTime();
 const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
 
-const Machine = () => {
+const Machine = ( {machines} ) => {
     return (
         <div>
             <Card>
