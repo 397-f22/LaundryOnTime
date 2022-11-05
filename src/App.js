b9960827d86ac894a02d02d10ca63633e2d476e4
Vishwani Sati <vishwanisati2022@u.northwestern.edu>
2022-11-05 01:47:22 -0400

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Machine from './components/Machine';
-import { useDbData } from './utilities/firebase';
-
-
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Machine from "./components/Machine";
+import { useDbData } from "./utilities/firebase";
+import Login from "./components/Login";
 function App() {
-  const [machines, error] = useDbData('/');
-
-  if (machines == undefined) return <h1>Loading...</h1>
-  if (error) return <h1>Error loading data</h1>
-
+  const [machines, error] = useDbData("/");
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (machines === undefined) return <h1>Loading...</h1>;
+  if (error) return <h1>Error loading data</h1>;
+  console.log(machines);
   return (
     <div className="App">
-      <Machine machines = {machines}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GuestRoute element={Login} />} />
+          <Route
+            path="/home"
+            element={user ? <Machine machines={machines} /> : <Login />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
+function GuestRoute({ element: Element, ...rest }) {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  return user ? <Navigate to="/home" /> : <Element />;
+}
+function PrivateRoute({ element: Element, ...rest }) {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const [machines, error] = useDbData("/");
+  return !user ? <Navigate to="/" /> : <Element />;
+}
 export default App;
--- src/components/Machine.js
+++ src/components/Machine.js
@@ -1,76 +1,162 @@
-import React, { Component } from 'react';
-import { Button, Card } from 'react-bootstrap';
-import { GiWashingMachine} from 'react-icons/gi'
-import { MdLocalLaundryService } from 'react-icons/md'
-import CountdownTimer from './Timer/CountdownTimer';
-import { useDbUpdate } from '../utilities/firebase';
-
+import React, { Component } from "react";
+import { Button, Card } from "react-bootstrap";
+import { GiWashingMachine } from "react-icons/gi";
+import { MdLocalLaundryService } from "react-icons/md";
+import CountdownTimer from "./Timer/CountdownTimer";
+import { useDbUpdate } from "../utilities/firebase";
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const updateMachineTime = (minutes, update) => {
-    const NOW_IN_MS = new Date().getTime();
-    const DURATION_IN_MS = minutes * 60 * 1000;
-    update({endTime: new Date(NOW_IN_MS + DURATION_IN_MS).toISOString()});
-}
+  const NOW_IN_MS = new Date().getTime();
+  const DURATION_IN_MS = minutes * 60 * 1000;
+  update({ endTime: new Date(NOW_IN_MS + DURATION_IN_MS).toISOString() });
+};
+const Machine = ({ machines }) => {
+  console.log(machines);
 
-const Machine = ( {machines} ) => {
-    const [updatewasher1, ] = useDbUpdate("/washer1");
-    const [updatewasher2, ] = useDbUpdate("/washer2");
-    const [updatedryer1, ] = useDbUpdate("/dryer1");
-    const [updatedryer2, ] = useDbUpdate("/dryer2");
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        console.log("Sign out successful");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log("An error happened");
+      });
+  };
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const [updatewasher1] = useDbUpdate("/washer1");
+  const [updatewasher2] = useDbUpdate("/washer2");
+  const [updatedryer1] = useDbUpdate("/dryer1");
+  const [updatedryer2] = useDbUpdate("/dryer2");
 
-
-    return (
+  return (
+    <div>
+      <div>
+        <h3>{user?.displayName}</h3>
+        <button onClick={handleLogOut}>Sign Out</button>
+      </div>
+      <Card>
+        Washer 1
+        <br />
+        <GiWashingMachine size={64} />
         <div>
-            <Card>
-                Washer 1
-                <br />
-                <GiWashingMachine size={64} />
-                <div>
-                    <CountdownTimer targetDate={Date.parse(machines["washer1"].endTime)} inUsage={Date.now() <= Date.parse(machines["washer1"].endTime)} />
-                </div>
-                {Date.now() > Date.parse(machines["washer1"].endTime) ? 
-                    <Button variant="success" onClick={() => {updateMachineTime(60, updatewasher1)}}>Start</Button>
-                    : <Button variant="danger" onClick={() => updateMachineTime(0, updatewasher1)}>Stop</Button> } 
-            </Card >
-            <br/>
-            <Card>
-                Washer 2
-                <br />
-                <GiWashingMachine size={64} />
-                <div>
-                    <CountdownTimer targetDate={Date.parse(machines["washer2"].endTime)} inUsage={Date.now() <= Date.parse(machines["washer2"].endTime)} />
-                </div>
-                {Date.now() > Date.parse(machines["washer2"].endTime) ? 
-                    <Button variant="success" onClick={() => {updateMachineTime(60, updatewasher2)}}>Start</Button>
-                    : <Button variant="danger" onClick={() => updateMachineTime(0, updatewasher2)}>Stop</Button> } 
-            </Card>
-            <br/>
-            <Card>
-                Dryer 1
-                <br />
-                <MdLocalLaundryService size={64} />
-                <div>
-                    <CountdownTimer targetDate={Date.parse(machines["dryer1"].endTime)} inUsage={Date.now() <= Date.parse(machines["dryer1"].endTime)} />
-                </div>
-                {Date.now() > Date.parse(machines["dryer1"].endTime) ? 
-                    <Button variant="success" onClick={() => {updateMachineTime(60, updatedryer1)}}>Start</Button>
-                    : <Button variant="danger" onClick={() => updateMachineTime(0, updatedryer1)}>Stop</Button> } 
-            </Card >
-            <br/>
-            <Card>
-                Dryer 2
-                <br />
-                <MdLocalLaundryService size={64} />
-                <div>
-                    <CountdownTimer targetDate={Date.parse(machines["dryer2"].endTime)} inUsage={Date.now() <= Date.parse(machines["dryer2"].endTime)} />
-                </div>
-                {Date.now() > Date.parse(machines["dryer2"].endTime) ? 
-                    <Button variant="success" onClick={() => {updateMachineTime(60, updatedryer2)}}>Start</Button>
-                    : <Button variant="danger" onClick={() => updateMachineTime(0, updatedryer2)}>Stop</Button> } 
-            </Card >
-        </div >
-    );
-}
-
+          <CountdownTimer
+            targetDate={Date.parse(machines["washer1"].endTime)}
+            inUsage={Date.now() <= Date.parse(machines["washer1"].endTime)}
+          />
+        </div>
+        {Date.now() > Date.parse(machines["washer1"].endTime) ? (
+          <Button
+            variant="success"
+            onClick={() => {
+              updateMachineTime(60, updatewasher1);
+            }}
+          >
+            Start
+          </Button>
+        ) : (
+          <Button
+            variant="danger"
+            onClick={() => updateMachineTime(0, updatewasher1)}
+          >
+            Stop
+          </Button>
+        )}
+      </Card>
+      <br />
+      <Card>
+        Washer 2
+        <br />
+        <GiWashingMachine size={64} />
+        <div>
+          <CountdownTimer
+            targetDate={Date.parse(machines["washer2"].endTime)}
+            inUsage={Date.now() <= Date.parse(machines["washer2"].endTime)}
+          />
+        </div>
+        {Date.now() > Date.parse(machines["washer2"].endTime) ? (
+          <Button
+            variant="success"
+            onClick={() => {
+              updateMachineTime(60, updatewasher2);
+            }}
+          >
+            Start
+          </Button>
+        ) : (
+          <Button
+            variant="danger"
+            onClick={() => updateMachineTime(0, updatewasher2)}
+          >
+            Stop
+          </Button>
+        )}
+      </Card>
+      <br />
+      <Card>
+        Dryer 1
+        <br />
+        <MdLocalLaundryService size={64} />
+        <div>
+          <CountdownTimer
+            targetDate={Date.parse(machines["dryer1"].endTime)}
+            inUsage={Date.now() <= Date.parse(machines["dryer1"].endTime)}
+          />
+        </div>
+        {Date.now() > Date.parse(machines["dryer1"].endTime) ? (
+          <Button
+            variant="success"
+            onClick={() => {
+              updateMachineTime(60, updatedryer1);
+            }}
+          >
+            Start
+          </Button>
+        ) : (
+          <Button
+            variant="danger"
+            onClick={() => updateMachineTime(0, updatedryer1)}
+          >
+            Stop
+          </Button>
+        )}
+      </Card>
+      <br />
+      <Card>
+        Dryer 2
+        <br />
+        <MdLocalLaundryService size={64} />
+        <div>
+          <CountdownTimer
+            targetDate={Date.parse(machines["dryer2"].endTime)}
+            inUsage={Date.now() <= Date.parse(machines["dryer2"].endTime)}
+          />
+        </div>
+        {Date.now() > Date.parse(machines["dryer2"].endTime) ? (
+          <Button
+            variant="success"
+            onClick={() => {
+              updateMachineTime(60, updatedryer2);
+            }}
+          >
+            Start
+          </Button>
+        ) : (
+          <Button
+            variant="danger"
+            onClick={() => updateMachineTime(0, updatedryer2)}
+          >
+            Stop
+          </Button>
+        )}
+      </Card>
+    </div>
+  );
+};
 
 export default Machine;
